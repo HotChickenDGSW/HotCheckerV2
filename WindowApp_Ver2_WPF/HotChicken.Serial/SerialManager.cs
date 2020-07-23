30ebd01608278d4b9a49b039b00c1b3beccb14b9
김태오 <38588097+Sunday5214@users.noreply.github.com>
2020-07-23 22:25:02 +0900

    ADD : 기본 레이아웃 작업

---
+++ WindowApp_Ver2_WPF/HotChicken.Serial/SerialManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotChicken.Serial
+{
+    public class SerialManager
+    {
+       
+        private string[] ports
+        {
+            get;
+            set;
+        }
+        private string portName
+        {
+            get;
+            set;
+        }
+        private int baudRate
+        {
+            get;
+            set;
+        }
+        private int dataBits
+        {
+            get;
+            set;
+        }
+        private StopBits StopBits
+        {
+            get;
+            set;
+        }
+        private Parity Parity
+        {
+            get;
+            set;
+        }
+        public SerialDataReceivedEventHandler SerialDataReceivedEventHandler
+        {
+            get;
+            set;
+        }
+        
+        public void GetSerialPorts()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+
+        public void ConnectSomePort()
+        {
+
+        }
+    }
+}
