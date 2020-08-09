143c34ad3a5b38319a67ff4b06c4fe79ed4f0cea
김태오 <38588097+Sunday5214@users.noreply.github.com>
2020-08-09 14:50:41 +0900

    wer

---
+++ WindowApp_Ver2_WPF/HotCheckerManager/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotCheckerManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NamedPipeServerStream pipeServer =
+            new NamedPipeServerStream("testpipe", PipeDirection.InOut))
+            {
+                Console.WriteLine("NamedPipeServerStream object created.");
+
+                // Wait for a client to connect
+                Console.Write("Waiting for client connection...");
+                pipeServer.WaitForConnection();
+
+                Console.WriteLine("Client connected.");
+                try
+                {
+                    // Read user input and send that to the client process.
+                    using (StreamWriter sw = new StreamWriter(pipeServer))
+                    {
+                        sw.AutoFlush = true;
+                        Console.Write("Enter text: ");
+                        sw.WriteLine(Console.ReadLine());
+                    }
+                }
+                // Catch the IOException that is raised if the pipe is broken
+                // or disconnected.
+                catch (IOException e)
+                {
+                    Console.WriteLine("ERROR: {0}", e.Message);
+                }
+            }
+
+            // string a = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+            // Process.Start(a);
+        }
+
+
+    }
+}
---
+++ WindowApp_Ver2_WPF/HotChecker_WPF/Service/PipeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotChecker_WPF.Service
+{
+    public class PipeService
+    {
+        public void PipeInit()
+        {
+            using (NamedPipeClientStream pipeClient =
+            new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut, PipeOptions.Asynchronous))
+            {
+
+                // Connect to the pipe or wait until the pipe is available.
+                Console.Write("Attempting to connect to pipe...");
+                pipeClient.Connect();
+
+                Console.WriteLine("Connected to pipe.");
+                Console.WriteLine("There are currently {0} pipe server instances open.",
+                   pipeClient.NumberOfServerInstances);
+                using (StreamReader sr = new StreamReader(pipeClient))
+                {
+                    // Display the read text to the console
+                    string temp;
+                    while ((temp = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine("Received from server: {0}", temp);
+                    }
+                }
+            }
+            Console.Write("Press Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
